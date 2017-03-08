47a58d17296367c0f0048fcc562aef04b064bcb3
William Velarde <williamvelarde@Williams-MacBook-Air.local>
2017-03-08 12:12:36 -0700

    Added categories

--- cms/blog/models.py
+++ cms/blog/models.py
@@ -4,21 +4,39 @@ from django.core.urlresolvers import reverse
 
 from django.contrib.auth.models import User
 
+# Category
+class Category(models.Model):
+	name = models.CharField(max_length=250)
+	slug = models.SlugField(max_length=250, unique=True)
+
+	class Meta:
+		ordering = ('name',)
+		verbose_name = 'category'
+		verbose_name_plural = 'catorgies'
+
+	def get_absolute_url(self):
+		return reverse('blog:list_of_post_by_category', args=[self.slug])
+
+	def __str__(self):
+		return self.name
+
+# Post
 class Post(models.Model):
 	STATUS_CHOICES = (
 		('draft', 'Draft'),
 		('published', 'Published'),
 	)
-	title = models.CharField(max_length = 250)
-	slug = models.SlugField(max_length = 250)
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=250)
+	slug = models.SlugField(max_length=250, unique=True)
 	content = models.TextField()
-	seo_title = models.CharField(max_length = 250)
-	seo_description = models.CharField(max_length = 160)
-	author = models.ForeignKey(User, related_name = 'blog_posts')
-	published = models.DateTimeField(default = timezone.now)
-	created = models.DateTimeField(auto_now_add = True)
-	updated = models.DateTimeField(auto_now = True)
-	status = models.CharField(max_length = 9, choices = STATUS_CHOICES, default = 'draft')
+	seo_title = models.CharField(max_length=250)
+	seo_description = models.CharField(max_length=160)
+	author = models.ForeignKey(User, related_name='blog_posts')
+	published = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=9, choices=STATUS_CHOICES, default='draft')
 
 	def get_absolute_url(self):
 		return reverse('blog:post_detail', args=[self.slug])
--- cms/blog/urls.py
+++ cms/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.list_of_post, name='list_of_post'),
-	url(r'^(?P<slug>[-\w]+)/$', views.post_detail, name='post_detail')
+	url(r'^(?P<slug>[-\w]+)/$', views.post_detail, name='post_detail'),
+	url(r'^category/(?P<category_slug>[-\w]+)/$', views.list_of_post_by_category, name='list_of_post_by_category'),
 ]
