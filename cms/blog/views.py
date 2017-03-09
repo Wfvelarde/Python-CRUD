290d5fe2c0f65c1814318aa6fe85f7b4eec0db4d
William Velarde <williamvelarde@Williams-MacBook-Air.local>
2017-03-08 17:50:28 -0700

    Added bootstrap styling as well as edit and delete features

--- cms/blog/views.py
+++ cms/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import Http404
+from django.contrib import messages
 from .models import Post, Category
 
-from .forms import CommentForm
+from .forms import CommentForm, PostForm
 
 def list_of_post_by_category(request, category_slug):
 	categories = Category.objects.all()
@@ -25,14 +27,18 @@ def list_of_post(request):
 	except EmptyPage:
 		posts = paginator.page(paginator.num_pages)
 	template = 'blog/post/list_of_post.html'
-	context = {'post': post, 'page': page}
+	context = {'posts': posts, 'page': page}
 	return render(request, template, context)
 
 def post_detail(request, slug):
 	post = get_object_or_404(Post, slug=slug)
-	template = 'blog/post/post_detail.html'
 	context = {'post': post}
-	return render(request, template, context)
+	if post.status == 'published':
+		template = 'blog/post/post_detail.html'
+		return render(request, template, context)
+	else:
+		template = 'blog/post/post_preview.html'
+		return render(request, template, context)
 
 def add_comment(request, slug):
 	post = get_object_or_404(Post, slug=slug)
@@ -48,4 +54,60 @@ def add_comment(request, slug):
 	template = 'blog/post/add_comment.html'
 	context = {'form': form}
 	return render(request, template, context)
+
+###########
+# BACKEND #
+###########
+
+def new_post(request):
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.save()
+			return redirect('blog:post_detail', slug=post.slug)
+	else:
+		form = PostForm()
+	template ='blog/backend/new_post.html'
+	context = {'form': form}
+	return render(request, template, context)
+
+def list_of_post_backend(request):
+	post = Post.objects.all()
+	paginator = Paginator(post, 20)
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+	template = 'blog/backend/list_of_post_backend.html'
+	context = {'posts': posts, 'page': page}
+	return render(request, template, context)
+
+def edit_post(request, slug):
+	post = get_object_or_404(Post, slug=slug)
+	if request.method == 'POST':
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.save()
+			return redirect('blog:list_of_post_backend')
+	else:
+		form = PostForm(instance=post)
+	template = 'blog/backend/new_post.html'
+	context = {'form': form}
+	return render(request, template, context)
+
+def delete_post(request, slug):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
+	post = get_object_or_404(Post, slug=slug)
+	post.delete()
+	messages.success(request, 'Successfully Deleted')
+	return redirect('blog:list_of_post_backend')
+
 
